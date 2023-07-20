32318058edc80f4b583911fd20cde82610d5e302
geektcp <geektcp@163.com>
2023-07-20 19:23:26 +0800

    review code: GenIdUtils

--- src/main/java/com/geektcp/common/spring/jpa/JpaRepo.java
+++ src/main/java/com/geektcp/common/spring/jpa/JpaRepo.java
@@ -2,14 +2,11 @@ package com.geektcp.common.spring.jpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 /**
  * @author Created by tanghaiyang on 2018/12/25.
  */
 @NoRepositoryBean
-public interface JpaRepo<T> extends JpaRepository<T, Long>,
-        JpaSpecificationExecutor<T>,
-        QueryDslPredicateExecutor<T> {
+public interface JpaRepo<T> extends JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
 }
--- src/main/java/com/geektcp/common/spring/util/GenIdUtils.java
+++ src/main/java/com/geektcp/common/spring/util/GenIdUtils.java
@@ -2,6 +2,7 @@ package com.geektcp.common.spring.util;
 
 import com.geektcp.common.mosheh.exception.BaseException;
 import com.geektcp.common.mosheh.generator.IdGenerator;
+import com.geektcp.common.mosheh.system.Sys;
 import com.geektcp.common.spring.model.po.Po;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
@@ -11,31 +12,49 @@ import javax.persistence.Table;
 
 @Slf4j
 public class GenIdUtils implements GenId<String> {
+
+    private static String defaultPre = "_";
+
+    public static void initPre(String pre){
+        defaultPre = pre;
+    }
+
     @Override
     public String genId(String table, String column) {
-        return getId(table);
+        return generateId(table);
     }
 
-    public static String getId(String tableName) {
-        String prefix = StringUtils.substringAfter(tableName, "des_");
+    public static String genId(String table) {
+        return genIdByTable(table);
+    }
+
+    public static String genIdByPre(String table, String pre) {
+        return generateId(table, pre);
+    }
+
+    public static <A extends Po> String getId(Class<A> poClass) {
+        String tableName = poClass.getAnnotation(Table.class).name();
+        String prefix = StringUtils.substringAfter(tableName, defaultPre);
         return IdGenerator.getId(prefix);
     }
 
-    public static <A extends Po> String getIdByClass(Class<A> poClass) {
-        String tableName = poClass.getAnnotation(Table .class).name();
-        String prefix = StringUtils.substringAfter(tableName, "des_");
+
+    /////////////////////
+    private static String genIdByTable(String table) {
+        return generateId(table, defaultPre);
+    }
+
+    private static String generateId(String tableName) {
+        String prefix = StringUtils.substringAfter(tableName, defaultPre);
         return IdGenerator.getId(prefix);
     }
 
-    public String genDefaultId(String table, String column) {
-        try {
-            String prefix = table;
-            Thread.sleep(1);
-            return IdGenerator.getId(prefix);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-            throw new BaseException(e);
-        }
+    private static String generateId(String tableName, String pre) {
+        String prefix = StringUtils.substringAfter(tableName, defaultPre);
+        return IdGenerator.getId(prefix);
     }
+
+
 
 }
+
